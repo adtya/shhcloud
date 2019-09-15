77a57723e655ac77e892ace1d0b7842ba03d8545
Adithya Nair <adtya.nair@gmail.com>
2019-09-15 23:29:10 +0530

    format with black

--- setup.py
+++ setup.py
@@ -4,31 +4,29 @@ from shhcloud import __version__
 
 def readme():
     try:
-        with open('README.md') as readme:
+        with open("README.md") as readme:
             return readme.read()
     except:
         pass
 
 
 setup(
-    name='shhcloud',
-    version='.'.join(str(i) for i in __version__),
-    description='create wordclouds based on shell history',
+    name="shhcloud",
+    version=".".join(str(i) for i in __version__),
+    description="create wordclouds based on shell history",
     author="Adithya Nair",
-    author_email='adtya.nair@gmail.com',
+    author_email="adtya.nair@gmail.com",
     long_description=readme(),
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://gitlab.com/adtya/shhcloud",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['wordcloud==1.5.0'],
-
-    entry_points={'console_scripts': ['shhcloud=shhcloud.main:main']},
+    install_requires=["wordcloud==1.5.0"],
+    entry_points={"console_scripts": ["shhcloud=shhcloud.main:main"]},
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-
-    ]
+    ],
 )
--- shhcloud/main.py
+++ shhcloud/main.py
@@ -7,10 +7,10 @@ from wordcloud import WordCloud
 
 
 def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
-    color = "rgb({0}, {1}, {2})".format(random.randint(128, 255),
-                                        random.randint(128, 255),
-                                        random.randint(128, 255))
-    return(color)
+    color = "rgb({0}, {1}, {2})".format(
+        random.randint(128, 255), random.randint(128, 255), random.randint(128, 255)
+    )
+    return color
 
 
 def main():
@@ -18,29 +18,39 @@ def main():
     img_width = 1920
     img_height = 1080
     working_dir = os.getcwd()
-    save_file = os.path.join(working_dir, 'wordcloud.jpg')
+    save_file = os.path.join(working_dir, "wordcloud.jpg")
     if arg_list != []:
         for loc, arg in enumerate(arg_list):
-            if arg == '-s':
+            if arg == "-s":
                 try:
-                    w = arg_list[loc+1]
-                    h = arg_list[loc+2]
+                    w = arg_list[loc + 1]
+                    h = arg_list[loc + 2]
                     if w.isnumeric() and h.isnumeric():
                         print(w.isnumeric, h.isnumeric)
                         img_width = w
                         img_height = h
                     else:
-                        printf("oops! width and height should be integers. the given values are ignored and",
-                               img_width, "and", img_height, "are used")
+                        printf(
+                            "oops! width and height should be integers. the given values are ignored and",
+                            img_width,
+                            "and",
+                            img_height,
+                            "are used",
+                        )
                 except:
-                    print("oops! width and height should be integers. the given values are ignored and",
-                          img_width, "and", img_height, "are used")
-            elif arg == '-f':
+                    print(
+                        "oops! width and height should be integers. the given values are ignored and",
+                        img_width,
+                        "and",
+                        img_height,
+                        "are used",
+                    )
+            elif arg == "-f":
                 try:
-                    f = arg_list[loc+1]
-                    if f[0] == '.' and f[1].isalnum:
+                    f = arg_list[loc + 1]
+                    if f[0] == "." and f[1].isalnum:
                         save_file = os.path.join(working_dir, f[1, -1])
-                    elif f[0] == '/':
+                    elif f[0] == "/":
                         save_file = f
                     elif f[0].isalnum:
                         save_file = os.path.join(working_dir, f)
@@ -49,10 +59,10 @@ def main():
             else:
                 pass
 
-    home_dir = os.environ['HOME']
+    home_dir = os.environ["HOME"]
     # Get directories stored in $PATH
-    command_dir = os.environ['PATH'].split(":")
-    shell = os.environ['SHELL']
+    command_dir = os.environ["PATH"].split(":")
+    shell = os.environ["SHELL"]
 
     valid_commands = []
     # Get names of all files immediately in all of the above directories and add to valid_commands
@@ -61,16 +71,22 @@ def main():
             valid_commands.append(valid_command.split("/")[-1])
 
     # Get shell's built in commands by running 'compgen -b' and add them to valid_commands
-    built_in_commands = os.popen('compgen -b').read().split('\n')
+    built_in_commands = os.popen("compgen -b").read().split("\n")
     valid_commands.extend(built_in_commands)
 
     # get aliases from shell
-    aliases = os.popen(shell + ' -i -c alias').read().split('\n')
-    if '' in aliases:
-        aliases.remove('')
+    aliases = os.popen(shell + " -i -c alias").read().split("\n")
+    if "" in aliases:
+        aliases.remove("")
 
-    aliases = {alias.replace('alias ', '').split('=')[0].replace("'", ''): alias.replace(
-        'alias ', '').split('=')[1].replace("'", '') for alias in aliases}
+    aliases = {
+        alias.replace("alias ", "")
+        .split("=")[0]
+        .replace("'", ""): alias.replace("alias ", "")
+        .split("=")[1]
+        .replace("'", "")
+        for alias in aliases
+    }
 
     # Open and read .bash_history
     bash_history = open(os.path.join(home_dir, ".bash_history"), "r")
@@ -88,7 +104,7 @@ def main():
             words.append(parent)
         if parent in aliases.keys():
             words.append(aliases[parent].split(" ")[0])
-        if (parent == "sudo"):
+        if parent == "sudo":
             # If parent command is sudo, get the child command (ie, for 'sudo mv' get 'mv')
             child = split_command[1]
             if child in valid_commands:
@@ -97,26 +113,32 @@ def main():
         # If pipes(|) exists in the command, get the command after each pipes
         for loc, word in enumerate(split_command):
             if word == "|":
-                if split_command[loc+1] in valid_commands:
-                    words.append(split_command[loc+1])
+                if split_command[loc + 1] in valid_commands:
+                    words.append(split_command[loc + 1])
                 # if it's a sudo, get the child command same as above
-                if split_command[loc+1] == 'sudo':
-                    words.append(split_command[loc+2])
+                if split_command[loc + 1] == "sudo":
+                    words.append(split_command[loc + 2])
 
     # Create a frequency table from the words list
     freq_table = Counter(words)
 
     # Do the magic with frequency table instead
-    wc = WordCloud(color_func=color_func, max_words=len(freq_table), width=int(img_width), height=int(
-        img_height)).generate_from_frequencies(freq_table)
+    wc = WordCloud(
+        color_func=color_func,
+        max_words=len(freq_table),
+        width=int(img_width),
+        height=int(img_height),
+    ).generate_from_frequencies(freq_table)
 
     # Save it to a file
     try:
         wc.to_file(save_file)
     except:
-        print("oops! something went wrong while writing to the file. Please check if the given path is writable")
+        print(
+            "oops! something went wrong while writing to the file. Please check if the given path is writable"
+        )
         exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
